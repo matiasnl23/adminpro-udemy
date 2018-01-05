abe880c602dac0db5b72a0ec9efca5d24c99cecc
Matías Lobo <matiasnl.1993@gmail.com>
2018-01-04 21:28:11 -0300

    Se agrega función para poner el check del tema usando javascript crudo

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -13,10 +13,21 @@ export class AccountSettingsComponent implements OnInit {
   ngOnInit() {
   }
 
-  cambiarTema( tema: string ) {
-    const url = `assets/css/colors/${tema}.css`;
+  cambiarTema( tema: string, link: any ) {
 
+    this.aplicarCheck(link);
+
+    const url = `assets/css/colors/${tema}.css`;
     this._document.getElementById('tema').setAttribute('href', url);
+  }
+
+  aplicarCheck(link: any) {
+    const selectores: any = document.getElementsByClassName('selector');
+    for (const ref of selectores) {
+      ref.classList.remove('working');
+    }
+
+    link.classList.add('working');
   }
 
 }
