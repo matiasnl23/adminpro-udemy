cf198444bce0f59fd701bb5494db642264491f55
Matías Lobo <matiasnl.1993@gmail.com>
2018-01-06 15:01:13 -0300

    Opción para modificar el título que figurará en la pestaña del navegador.

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -11,11 +12,15 @@ export class BreadcrumbsComponent implements OnInit {
   titulo: string = 'Cargando...';
 
   constructor(
-    private router: Router
+    private router: Router,
+    public _title: Title
   ) {
     
     this.getDataRoute().subscribe( data => {
       this.titulo = data.titulo;
+
+      // Cambiando el título que figura en la pestaña del navegador
+      this._title.setTitle(this.titulo);
     });
   }
 
