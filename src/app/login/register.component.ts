f9b47b488a848bae2600e74c26481220deb5d86d
Matías Lobo <matiasnl.1993@gmail.com>
2018-01-08 00:54:27 -0300

    ReactiveForms, Inicialización de plugins. Uso de FormGroup para el manejo del formulario, FormControl para los inputs del formulario, 'Validators.required' y 'Validators.Email', Validación personalizada para la igualación de las contraseñas. Método para el ngSubmit.

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+declare function init_plugins();
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  forma: FormGroup;
+  
   constructor() { }
 
   ngOnInit() {
+    init_plugins();
+
+    this.forma = new FormGroup({
+      nombre: new FormControl( null, [Validators.required]),
+      correo: new FormControl( null, [Validators.required, Validators.email]),
+      password: new FormControl( null, [Validators.required]),
+      password2: new FormControl( null, [Validators.required]),
+      condiciones: new FormControl( false )
+    }, { validators: this.camposIguales( 'password', 'password2' ) } );
+  }
+
+  camposIguales( campo_1: string, campo_2: string) {
+    return ( group: FormGroup) => {
+      let c1 = group.controls[campo_1].value;
+      let c2 = group.controls[campo_2].value;
+
+      if(c1 === c2) {
+        return null;
+      }
+
+      return { camposIguales: true };
+    }
+  }
+
+  registrarUsuario() {
+    console.log( this.forma.value );
+    console.log( this.forma.valid );
   }
 
 }
