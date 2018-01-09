40ac784891bc527c38eee2a975a9c7b8f3c944fa
Matías Lobo <matiasnl.1993@gmail.com>
2018-01-08 21:38:20 -0300

    Se realizan las modificaciones para a través del map, guardar en el localStorage los datos de logueo. También se guarda en el localStorage si se quiere, el email.

--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -14,10 +14,23 @@ export class UsuarioService {
   }
 
   login( usuario: Usuario, recordar: boolean = false ) {
+
+    if(recordar) {
+      localStorage.setItem('email', usuario.email);
+    } else {
+      localStorage.removeItem('email');
+    }
     
     const url = `${ URL_SERVICIOS }/login`;
 
-    return this.http.post( url, usuario );
+    return this.http.post( url, usuario ).map( (resp: any) => {
+
+      localStorage.setItem('id', resp.id );
+      localStorage.setItem('token', resp.token );
+      localStorage.setItem('usuario', JSON.stringify(resp.usuario));
+
+      return true;
+    });
     
   }
 
