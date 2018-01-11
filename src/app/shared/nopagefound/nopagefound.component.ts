6c35695f24427d82bafde941e74dcefe6fd804ae
Matías Lobo <matiasnl.1993@gmail.com>
2018-01-11 07:01:08 -0300

    Se ejecuta el init_plugins(); por si se recarga en esta página.

--- src/app/shared/nopagefound/nopagefound.component.ts
+++ src/app/shared/nopagefound/nopagefound.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function init_plugins();
+
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NopagefoundComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    init_plugins();
   }
 
 }
