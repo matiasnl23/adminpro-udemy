35f9d9293225d37810ac89e587dd90f33140dd90
Matías Lobo <matiasnl.1993@gmail.com>
2018-01-10 22:40:27 -0300

    En el template se crea la nueva imagen que contendrá la imagen temporal a modificar. En el script se valida la imagen y se la asigna a la variable como una imagen en base64 que será mostrada como vista previa. SE UTILIZA 'FileReader()' CON JAVASCRIPT PURO PARA VER LA IMAGEN.

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -11,6 +11,7 @@ export class ProfileComponent implements OnInit {
 
   usuario: Usuario;
   imagenSubir: File;
+  imagenTemp: string;
 
   constructor(
     public _usuarioService: UsuarioService
@@ -27,7 +28,18 @@ export class ProfileComponent implements OnInit {
       return;
     }
 
+    if(archivo.type.indexOf('image')<0) {
+      swal('Imagen inválida', 'El archivo seleccionado no es una imagen', 'error');
+      this.imagenSubir = null;
+      return;
+    }
+
     this.imagenSubir = archivo;
+
+    let reader = new FileReader();
+    let urlImagenTemp = reader.readAsDataURL(archivo);
+
+    reader.onloadend = () => this.imagenTemp = reader.result;
   }
 
   cambiarImagen() {
